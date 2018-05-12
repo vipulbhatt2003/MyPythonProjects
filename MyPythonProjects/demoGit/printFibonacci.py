d0bea6027544838642c360890ef9b003e6d38bae
bhattvip <bhattvip@BHATTVIP1.asiapacific.hpqcorp.net>
2018-05-13 02:27:31 +0530

    Committing the Fibonaaci and Golden Ratio Project

---
+++ MyPythonProjects/demoGit/printFibonacci.py
@@ -0,0 +1,31 @@
+'''
+Created on May 13, 2018
+
+@author: bhattvip
+'''
+import os
+import datetime
+
+a=0
+b=1
+c=1
+g=0
+counter=0
+print ('Fibonaaci Series'  , file=open('C:\\Git_Testing\\Fibonaaci\\fibonacci.txt', 'w') )
+
+def nextfibonaccinumber(x,y):
+    return x + y
+
+def goldenratio(x,y):
+    return y / x
+
+while counter <= 20:
+    now = datetime.datetime.now()
+    #print(c,'and', g)
+    print ('@Time' , str(now) , ' --> ',  c,' and Golden Ratio is', g , file=open('C:\\Git_Testing\\Fibonaaci\\fibonacci.txt', 'a') )
+    c=nextfibonaccinumber(a,b)
+    if a > 1:
+        g=goldenratio(a,b)
+    a=b
+    b=c
+    counter += 1
